5d1f5f4de479d8935d2e4e0013fe82ffdec6bfda
jayraj <jayrajvkpatil5142@gmail.com>
2024-11-19 12:04:08 +0530

    jay

---
+++ mng.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+	int age ;
+	printf("enter your age");
+	scanf("%d",&age);
+	 
+	 switch (age)
+	 {
+	 
+	 	case 3:
+	 		printf("the age is 3");
+	 	break;
+	 	
+	 	case 13:
+	 	printf("the age is 13");
+	 	break;
+	 	
+	 	case 33:
+	 	printf("the age is 33");
+	 	break;
+	 	
+	 	default:
+	 	printf("age is not 3 ,13, 33");
+	 	break;
+	 	
+	 }
+	 	
+	 	
+	 
+	
+	
+	
+	
+	
+	
+	return 0;
+	
+}
